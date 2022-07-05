fbc358c72ac7db3cdff09203e99cd86cd898920f
unknown <cristian.forero-m@labinfo.is.escuelaing.edu.co>
2022-07-05 12:25:16 -0500

    fin trabajo 5-7-2022

---
+++ src/main/java/co/edu/escuelaing/connect4/Connect4Configurator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.escuelaing.connect4;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ *
+ * @author cristian.forero-m
+ */
+@Configuration
+@EnableScheduling
+public class Connect4Configurator {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
